61d1c991ceb563b908e739a57fc7d71ea854c7ce
zernovga <zernovga@gmail.com>
2025-12-02 17:49:25 +0300

    refactor: add config

---
+++ common/config.py
@@ -0,0 +1,26 @@
+from pydantic_settings import BaseSettings, SettingsConfigDict
+
+
+class CommonSettings(BaseSettings):
+    model_config = SettingsConfigDict(
+        env_file=".env",  # можно поддерживать .env
+        env_file_encoding="utf-8",
+        extra="ignore",
+    )
+
+
+class KafkaSettings(CommonSettings):
+    bootstrap_servers: str = "kafka:9092"
+    topic_raw: str = "market.raw"
+
+
+class StreamSourceSettings(CommonSettings):
+    source_url: str = "wss://example.com/market-stream"
+
+
+class Settings(CommonSettings):
+    kafka: KafkaSettings = KafkaSettings()
+    stream: StreamSourceSettings = StreamSourceSettings()
+
+
+settings = Settings()
